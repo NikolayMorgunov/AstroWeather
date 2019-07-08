ef2553223905ad8a37ff95133574dc18017e1080
nikolaymorgunov <nikolamorgunov@mail.ru>
2019-07-08 23:12:03 +1000

    All without forecast

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ from users_db import *
 from geocoding import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'twit_twit'
-
-app = Flask(__name__)
 app.config['SECRET_KEY'] = 'i_love_astronomy'
 
 User.create_table()
@@ -33,11 +30,17 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Sign up')
 
 
+class ChangeLocation(FlaskForm):
+    country = StringField('Country', validators=[DataRequired()])
+    city = StringField('City', validators=[DataRequired()])
+    submit = SubmitField('Change')
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'username' in session:
-        return redirect('/main_page')
+        return redirect('/forecast')
     form = LoginForm()
     username = form.data['username']
     password = form.data['password']
@@ -45,11 +48,17 @@ def login():
     if form.validate_on_submit():
         if auth_check(username, password):
             session['username'] = username
-            return redirect('/main_page')
+            return redirect('/forecast')
         else:
             normal_auth = False
     return render_template('login.html', title='Login', form=form, normal_auth=normal_auth,
                            loginned='username' in session)
+
+
+@app.route('/log_out', methods=['GET', 'POST'])
+def logout():
+    session.pop('username', 0)
+    return redirect('/login')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -82,9 +91,55 @@ def register():
                            loginned='username' in session)
 
 
+@app.route('/success_register', methods=['GET', 'POST'])
+def success_register():
+    if 'username' in session:
+        return render_template('success_register.html', title='Register success', loginned='username' in session,
+                               username=session['username'])
+    else:
+        return render_template('success_register.html', title='Register success', loginned='username' in session)
+
+
 @app.route('/about', methods=['GET', 'POST'])
 def about():
-    return render_template('about.html', title='About', loginned='username' in session)
+    if 'username' in session:
+        return render_template('about.html', title='About', loginned='username' in session,
+                               username=session['username'])
+    else:
+        return render_template('about.html', title='About', loginned='username' in session)
+
+
+@app.route('/change_location', methods=['GET', 'POST'])
+def change_location():
+    form = ChangeLocation()
+    location_exists = True
+    country = form.data['country']
+    city = form.data['city']
+    if form.validate_on_submit():
+        if not exist(country, city):
+            location_exists = False
+        else:
+            coords_of_city = coords(country, city)
+            longitude = coords_of_city[0]
+            latitude = coords_of_city[1]
+            user = User.get(User.username == session['username'])
+            user.longitude = longitude
+            user.latitude = latitude
+            user.save()
+            return redirect("/success_change")
+    return render_template('change_location.html', title='Change location', form=form,
+                           location_not_exist=not location_exists,
+                           loginned='username' in session, username=session['username'])
+
+
+@app.route('/success_change', methods=['GET', 'POST'])
+def success_change():
+    if 'username' in session:
+        return render_template('success_change.html', title='Change success', loginned='username' in session,
+                               username=session['username'])
+    else:
+        return render_template('success_change.html', title='Change success', loginned='username' in session)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
